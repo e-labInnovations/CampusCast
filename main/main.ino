886ce7b2879962803e170878a7f4f92acc9ebf7a
e-labInnovations <mohammedashad23@gmail.com>
2023-06-14 20:02:15 +0530

    server configuration added

--- main/main.ino
+++ main/main.ino
@@ -3,69 +3,76 @@
 #include <ArduinoJson.h>  // https://github.com/bblanchon/ArduinoJson
 #include <SPIFFS.h>
 #include <WebSocketsClient.h>  //version 2.3.6
-
 #include "WiFi.h"
 #include "Audio.h"
+
+#define SSID_PREFIX "CampusCast_"
+#define PASSWORD "password"
+#define JSON_CONFIG_FILE "/config.json"
 
 #define I2S_DOUT 15  //25
 #define I2S_BCLK 13  //27
 #define I2S_LRC 12   //26
 
-// const char* ssid = "e-labinnovations";
-// const char* password = "PASSWORD";
-
 Audio audio;
 WiFiManager wm;
 WebSocketsClient webSocket;
 
-
-
-char classroom_code[10];
-
 bool shouldSaveConfig = true;
+char classroom_code_val[10] = "000";
+char ws_server_val[21] = "192.168.x.x";
+int ws_server_port_val = 1880;
+char ws_server_path_val[41] = "/";
 
-//callback notifying us of the need to save config
-void saveConfigCallback() {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
-}
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
 
-void setupSpiffs() {
-  // Uncomment if we need to format filesystem
-  // SPIFFS.format();
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(21);
 
-  Serial.println("mounting FS...");
+  bool forceConfig = false;  // Change to true when testing to force configuration every time we run
 
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json");
-      if (configFile) {
-        Serial.println("Opened configuration file");
-        StaticJsonDocument<512> json;
-        DeserializationError error = deserializeJson(json, configFile);
-        serializeJsonPretty(json, Serial);
-        if (!error) {
-          Serial.println("Parsing JSON");
-
-          strcpy(classroom_code, json["classroom_code"]);
-        } else {
-          // Error loading JSON data
-          Serial.println("Failed to load json config");
-        }
-      }
-    }
-  } else {
-    Serial.println("failed to mount FS");
+  bool spiffsSetup = loadConfigFile();
+  if (!spiffsSetup) {
+    Serial.println(F("Forcing config mode as there is no saved config"));
+    forceConfig = true;
   }
-  //end read
+
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+  setupWiFiManager();
+
+  // server address, port and URL
+  webSocket.begin(ws_server_val, ws_server_port_val, ws_server_path_val);
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  // try ever 5000 again if connection has failed
+  webSocket.setReconnectInterval(5000);
+  // start heartbeat (optional)
+  // ping server every 15000 ms
+  // expect pong from server within 3000 ms
+  // consider connection disconnected if pong is not received 2 times
+  webSocket.enableHeartbeat(15000, 3000, 2);
+
+  Serial.print("WS server: ");
+  Serial.println(ws_server_val);
+  Serial.print("WS server port: ");
+  Serial.println(ws_server_port_val);
+  Serial.print("WS server path: ");
+  Serial.println(ws_server_path_val);
+  Serial.print("Classroom Code: ");
+  Serial.println(classroom_code_val);
+
+  // audio.connecttohost("http://vis.media-ice.musicradio.com/CapitalMP3");
+  // audio.connecttohost("https://firebasestorage.googleapis.com/v0/b/campuscast-elabins.appspot.com/o/recording-test01.m4a?alt=media&token=2e25980f-1d7c-4675-9a52-67a0c65170fb");
 }
 
+void loop() {
+  // audio.loop();
+  webSocket.loop();
+}
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
-
   switch (type) {
     case WStype_DISCONNECTED:
       Serial.printf("[WSc] Disconnected!\n");
@@ -74,7 +81,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
       Serial.printf("[WSc] Connected to url: %s\n", payload);
 
       // send message to server when Connected
-      webSocket.sendTXT("{ \"command\":\"connected\", \"classroom_code\": \"" + String(classroom_code) + "\" }");
+      webSocket.sendTXT("{ \"command\":\"connected\", \"classroom_code\": \"" + String(classroom_code_val) + "\" }");
       break;
     case WStype_TEXT:
       Serial.printf("[WSc] get text: %s\n", payload);
@@ -96,69 +103,148 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
       break;
   }
 }
-void setup() {
 
-  //  wm.resetSettings();
+void saveConfigFile() {
+  Serial.println(F("Saving configuration..."));
 
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  setupSpiffs();
+  // Create a JSON document
+  StaticJsonDocument<512> json;
+  json["ws_server"] = ws_server_val;
+  json["ws_server_port"] = ws_server_port_val;
+  json["ws_server_path"] = ws_server_path_val;
+  json["classroom_code"] = classroom_code_val;
+
+  // Open config file
+  File configFile = SPIFFS.open(JSON_CONFIG_FILE, "w");
+  if (!configFile) {
+    Serial.println("failed to open config file for writing");
+  }
+
+  // Serialize JSON data to write to file
+  serializeJsonPretty(json, Serial);
+  if (serializeJson(json, configFile) == 0) {
+    Serial.println(F("Failed to write to file"));
+  }
+  // Close file
+  configFile.close();
+}
+
+// Load existing configuration file
+bool loadConfigFile() {
+  // Uncomment if we need to format filesystem
+  // SPIFFS.format();
+
+  // Read configuration from FS json
+  Serial.println("Mounting File System...");
+
+  // May need to make it begin(true) first time you are using SPIFFS
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists(JSON_CONFIG_FILE)) {
+      // The file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open(JSON_CONFIG_FILE, "r");
+      if (configFile) {
+        Serial.println("Opened configuration file");
+        StaticJsonDocument<512> json;
+        DeserializationError error = deserializeJson(json, configFile);
+        serializeJsonPretty(json, Serial);
+        if (!error) {
+          Serial.println("Parsing JSON");
+
+          strcpy(ws_server_val, json["ws_server"]);
+          ws_server_port_val = json["ws_server_port"].as<int>();
+          strcpy(ws_server_path_val, json["ws_server_path"]);
+          strcpy(classroom_code_val, json["classroom_code"]);
+          return true;
+        } else {
+          // Error loading JSON data
+          Serial.println("Failed to load json config");
+        }
+      }
+    }
+  } else {
+    // Error mounting file system
+    Serial.println("Failed to mount FS");
+  }
+  return false;
+}
+
+// Callback notifying us of the need to save configuration
+void saveConfigCallback() {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+// Called when config mode launched
+void configModeCallback(WiFiManager *myWiFiManager) {
+  Serial.println("Entered Configuration Mode");
+
+  Serial.print("Config SSID: ");
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+
+  Serial.print("Config IP Address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+
+void setupWiFiManager() {
+  // wm.resetSettings(); // reset settings - wipe stored credentials for testing
+  wm.setSaveConfigCallback(saveConfigCallback);  // Set config save notify callback
+  wm.setAPCallback(configModeCallback);          // Set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wm.setDebugOutput(false);                      //Disable debugging
+  wm.setDarkMode(true);                          // dark theme
+
+  WiFiManagerParameter ws_server("ws_server", "Websocket Server Address", ws_server_val, 20);
+  wm.addParameter(&ws_server);
+
+  char ws_server_port_val_a[16];
+  WiFiManagerParameter ws_server_port("ws_server_port", "Websocket Server Port", itoa(ws_server_port_val, ws_server_port_val_a, 10), 5);
+  wm.addParameter(&ws_server_port);
+
+  WiFiManagerParameter ws_server_path("ws_server_path", "Websocket Server Path", ws_server_path_val, 40);
+  wm.addParameter(&ws_server_path);
+
+  WiFiManagerParameter classroom_code("classroom_code", "Classroom No", classroom_code_val, 10);
+  wm.addParameter(&classroom_code);
+
   bool res;
-
-  wm.setSaveConfigCallback(saveConfigCallback);
-  WiFiManagerParameter custom_classroom_code("classroom_code", "Classroom No", classroom_code, 10);
-
-  wm.addParameter(&custom_classroom_code);
-
-  res = wm.autoConnect("CampusCast", "password");  // password protected ap
+  String AP_NAME = SSID_PREFIX + String(classroom_code_val);
+  res = wm.autoConnect(AP_NAME.c_str(), PASSWORD);  // password protected ap
 
   if (!res) {
     Serial.println("Failed to connect");
-    // ESP.restart();
+    ESP.restart();
   } else {
     //if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
+    // pcf8574.digitalWrite(LED_WiFi_OK, LOW); //Turning ON
+    // pcf8574.digitalWrite(LED_WiFi_ERROR, HIGH); //Turning OFF
   }
-  strcpy(classroom_code, custom_classroom_code.getValue());
 
-  //save the custom parameters to FS
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  strncpy(ws_server_val, ws_server.getValue(), sizeof(ws_server_val));                 // Copy the string value
+  ws_server_port_val = atoi(ws_server_port.getValue());                                //Convert the number value
+  strncpy(ws_server_path_val, ws_server_path.getValue(), sizeof(ws_server_path_val));  // Copy the string value
+  strcpy(classroom_code_val, classroom_code.getValue());
+
+  Serial.print("WS server: ");
+  Serial.println(ws_server.getValue());
+  Serial.print("WS server port: ");
+  Serial.println(ws_server_port.getValue());
+  Serial.print("WS server path: ");
+  Serial.println(ws_server_path.getValue());
+  Serial.print("Classroom Code: ");
+  Serial.println(classroom_code.getValue());
+  // Save the custom parameters to FS
   if (shouldSaveConfig) {
-    Serial.println("saving config");
-    StaticJsonDocument<512> json;
-    json["classroom_code"] = classroom_code;
-
-    // json["ip"]          = WiFi.localIP().toString();
-    // json["gateway"]     = WiFi.gatewayIP().toString();
-    // json["subnet"]      = WiFi.subnetMask().toString();
-
-    File configFile = SPIFFS.open("/config.json", FILE_WRITE);
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-    // Serialize JSON data to write to file
-    serializeJsonPretty(json, Serial);
-    if (serializeJson(json, configFile) == 0) {
-      Serial.println(F("Failed to write to file"));
-    }
-    configFile.close();
-    //end save
-    shouldSaveConfig = false;
+    saveConfigFile();
   }
-
-  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-
-  webSocket.begin("192.168.1.47", 1880, "/");
-  webSocket.onEvent(webSocketEvent);
-  webSocket.setReconnectInterval(5000);
-
-  audio.setVolume(21);
-  // audio.connecttohost("http://vis.media-ice.musicradio.com/CapitalMP3");
-  // audio.connecttohost("https://firebasestorage.googleapis.com/v0/b/campuscast-elabins.appspot.com/o/recording-test01.m4a?alt=media&token=2e25980f-1d7c-4675-9a52-67a0c65170fb");
-}
-
-void loop() {
-  // audio.loop();
-  webSocket.loop();
 }
 
 // optional
